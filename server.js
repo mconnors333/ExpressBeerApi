9e88bf13a35a67dbb328ca551f6f8654f90ba374
mconnors333 <matthew.connors333@gmail.com>
2017-01-13 16:29:32 -0500

    Fixed this stupid shit

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var mongoose = require("mongoose");
 var mongoURI = process.env.MONGODB_URI || "mongodb://localhost/beer_api";
 mongoose.connect(mongoURI);
 
-var Beer = require("./app/model/beer");
+var exphbs = require('express-handlebars');
+
+var Beer = require("./model/beer");
 
 
 // BASE SETUP
@@ -14,6 +16,9 @@ var Beer = require("./app/model/beer");
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // configure app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -30,6 +35,9 @@ router.use(function(req, res, next) {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.render("beer/index");
+});
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
